1447b080bcfaa9e2eea6b0eadad062c3aee1bcba
lotyp <lotyp7@gmail.com>
2022-03-30 23:41:45 +0300

    feat: added new file menu cask

---
+++ Casks/ssh-config-editor-alt.rb
@@ -0,0 +1,27 @@
+cask "ssh-config-editor-alt" do
+  version "2.3.2,91"
+  sha256 :no_check
+
+  url "https://www.dropbox.com/s/mh49vwvue47au8a/SSH.Config.Editor.v2.3.2.dmg.zip?dl=1",
+      user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0",
+      referer: "https://www.dropbox.com/s/mh49vwvue47au8a/SSH.Config.Editor.v2.3.2.dmg.zip?dl=1"
+  name "SSH Config Editor"
+  desc "Tool for managing the OpenSSH ssh client configuration file"
+  homepage "https://www.hejki.org/ssheditor/"
+
+  livecheck do
+    url "https://hejki.org/download/ssheditor/appcast#{version.major}.xml"
+    strategy :sparkle
+  end
+
+  auto_updates false
+  depends_on macos: ">= :catalina"
+
+  app "SSH Config Editor.app"
+
+  zap trash: [
+    "~/Library/Application Support/SSH Config Editor",
+    "~/Library/Preferences/org.hejki.osx.sshce.plist",
+    "~/Library/Saved Application State/org.hejki.osx.sshce.savedState",
+  ]
+end
