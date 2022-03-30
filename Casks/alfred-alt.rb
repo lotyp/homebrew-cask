498bc9db0d5d84d26b7443450df428fd566b60af
lotyp <lotyp7@gmail.com>
2022-03-30 20:11:15 +0300

    feat: added alfred

---
+++ Casks/alfred-alt.rb
@@ -0,0 +1,36 @@
+cask "alfred-alt" do
+  version "4.6.5,1298"
+  sha256 :no_check
+
+  url "https://www.dropbox.com/s/ra95z0nye53lb03/Alfred.4.6.5.Build.1298.dmg?dl=1",
+      user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0",
+      referer: "https://www.dropbox.com/s/ra95z0nye53lb03/Alfred.4.6.5.Build.1298.dmg?dl=1"
+  name "Alfred"
+  desc "Application launcher and productivity software"
+  homepage "https://www.alfredapp.com/"
+
+  livecheck do
+    url "https://www.alfredapp.com/app/update#{version.major}/general.xml"
+    strategy :page_match do |page|
+      match = page.match(/Alfred[._-]v?(\d(?:\.\d+)+)[._-](\d+)\.tar\.gz/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  auto_updates false
+
+  app "Alfred #{version.major}.app"
+
+  uninstall quit: "com.runningwithcrayons.Alfred"
+
+  zap trash: [
+    "~/Library/Application Support/Alfred",
+    "~/Library/Caches/com.runningwithcrayons.Alfred",
+    "~/Library/Cookies/com.runningwithcrayons.Alfred.binarycookies",
+    "~/Library/Preferences/com.runningwithcrayons.Alfred.plist",
+    "~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist",
+    "~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState",
+  ]
+end
