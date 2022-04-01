5af2e28bbe50e205af340598e2d1af64f85947f9
lotyp <lotyp7@gmail.com>
2022-04-01 18:02:17 +0300

    feat: add focus-alt cask

---
+++ Casks/focus.alt.rb
@@ -0,0 +1,30 @@
+cask "focus-alt" do
+  version "1.13.1"
+  sha256 :no-check
+
+  url "https://www.dropbox.com/s/zgzm7n9vh0yjeh2/Focus.#{version}.dmg.zip?dl=1",
+      user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0",
+      referer: "https://www.dropbox.com/"
+  url "https://heyfocus.com/uploads/Focus-v#{version}.zip"
+  name "Focus"
+  desc "Website and application blocker"
+  homepage "https://heyfocus.com/"
+
+  livecheck do
+    url "https://heyfocus.com/focus.zip"
+    strategy :header_match
+  end
+
+  conflicts_with cask: "focus"
+  depends_on macos: ">= :yosemite"
+
+  app "Focus.app"
+
+  uninstall quit: "BradJasper.focus"
+
+  zap trash: [
+    "~/Library/Application Support/Focus/",
+    "~/Library/Caches/BradJasper.focus/",
+    "~/Library/Preferences/BradJasper.focus.plist",
+  ]
+end
